4c26b7b51b95cc224c337c6603877fc8366ea65b
atomichorror <klucyfera@gmail.com>
2016-03-06 15:57:34 +0100

    Create exercise4.c

---
+++ s.p-C-6th_ed./Chapter2/C/exercise4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// 1 year = 12 months
+
+int main(){
+    int years;
+    printf("How old are you? ");
+    scanf("%d",&years);
+    printf("\n%d is %d months.", years, years*12);
+
+    return 0;
+}
