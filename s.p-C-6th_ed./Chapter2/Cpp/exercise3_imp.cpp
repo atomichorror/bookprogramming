e4acf1b1da2a7c06042ffb58759fb60c648276e8
atomichorror <klucyfera@gmail.com>
2016-03-06 16:02:19 +0100

    exercise3_imp.cpp

---
+++ s.p-C-6th_ed./Chapter2/Cpp/exercise3_imp.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void ent(){
+    for(int i=0;i<2;i++){
+        cout << "Entliczek pentliczek" << endl;
+    }
+}
+void st(){
+    for(int i=0;i<2;i++){
+        cout << "Czerwony stoliczek" << endl;
+    }
+}
+
+int main(){
+    ent();
+    st();
+
+    return 0;
+}
