7684f05981b69ee25f7eb36987a61cfdadb94265
atomichorror <klucyfera@gmail.com>
2016-03-06 15:56:50 +0100

    exercise3_imp.c

---
+++ s.p-C-6th_ed./Chapter2/C/exercise3_imp.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void ent(){
+    int i;
+    for(i=0;i<2;i++){
+        printf("Entliczek pentliczek\n");
+    }
+}
+void st(){
+    int i;
+    for(i=0;i<2;i++){
+        printf("Czerwony stoliczek\n");
+    }
+}
+
+int main(){
+    ent();
+    st();
+
+    return 0;
+}
